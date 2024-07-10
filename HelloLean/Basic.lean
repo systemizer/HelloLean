dd9a46a87d5835a645e877db9a44889fb6b95527
Rob McQueen <robmcqn@gmail.com>
2024-07-09 20:03:02 -0400

    Add Transitivity Example

--- HelloLean/Basic.lean
+++ HelloLean/Basic.lean
@@ -1,1 +1,7 @@
-def hello := "world"
+
+-- This basic example demonstrates transitivity of implication
+example {A B C: Prop} (h1: A → B) (h2: B → C) : A → C := by
+  intro ha
+  have hb : B := (h1 ha)
+  have hc : C := (h2 hb)
+  exact hc
